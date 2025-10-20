c8ceb702ce5622f1c5e447e073267520360b110d
Hypatia03 <camly6937@gmail.com>
2025-10-20 15:15:07 +0700

    Add files via upload

---
+++ CapNhatThongTin.sql
@@ -0,0 +1,13 @@
+﻿create proc CapNhatThongTin
+    @maNXB char(10),
+    @tenNXB nvarchar(100),
+    @diaChi nvarchar(500)
+as
+begin
+    update NhaXuatBan
+    set
+        NXB = @maNXB,
+        TenNXB = @tenNXB,
+        DiaChi = @diaChi
+    where NXB = @maNXB
+end
---
+++ ThemDuLieu.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE ThemDuLieu
+    @maXB CHAR(10),
+    @tenXB NVARCHAR(100),
+    @diaChi NVARCHAR(500)
+AS
+BEGIN
+    INSERT INTO NhaXuatBan
+    VALUES (@maXB, @tenXB, @diaChi);
+END 
